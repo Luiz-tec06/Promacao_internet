1d7369aaf8e76acb6bb69c2542b7b1150b8b0366
Luiz-tec06 <luiz60707@edu.unipar.br>
2025-05-21 20:20:48 -0300

    Add files via upload
    lista 2

--- script1.js
+++ script1.js
@@ -1,7 +1,29 @@
-function somar() {
-let numero1 = parseFloat(document.getElementById("num1").value);
- let numero2 = parseFloat(document.getElementById("num2").value);
-let resultado = numero1 + numero2;
+let Reajuste = document.querySelector("#Reajuste");
+let Caixa = document.querySelector("#Caixa");
+let Um = document.querySelector("#Um");
+let Dois = document.querySelector("#Dois");
+let Cinco = document.querySelector("#Cinco");
+let Dez = document.querySelector("#Dez");
 
-document.getElementById("resultado").innerText = "Resultado: " + resultado;
+function Reajustar(porcentagem){
+    let valor = Number (Caixa.value)
+    let valorreajustado = (valor * (porcentagem / 100)) + valor
+
+    Reajuste.textContent = valorreajustado;
+}
+
+Um.onclick = function(){
+    Reajustar(1);
+}
+
+Dois.onclick = function(){
+    Reajustar(2);
+}
+
+Cinco.onclick = function(){
+    Reajustar(5);
+}
+
+Dez.onclick = function(){
+    Reajustar(10);
 }
